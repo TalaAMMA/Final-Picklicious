90f8ac5c1de3957cb95ae0d66018ecea8139b1c8
Tala Ammar <talaammar@Talas-Laptop.local>
2026-03-23 14:36:25 +0100

    Fix: final path corrections and script automation

--- picklicious-front-end/prod/js/crypto.js
+++
@@ -1,8 +0,0 @@
-import F from "node-forge";
-
-export const hash=(toHash)=>{
-    toHash= toHash.replace(/\s/g, "");
-    const md =F.md.sha512.create();
-    md.update(toHash);
-    return md.digest().toHex();
-}
--- picklicious-front-end/prod/js/string.util.js
+++
@@ -1,2 +0,0 @@
-export const isString = (data) => typeof data === "string";
-export const stringIsFilled = (data) => isString(data) && data.trim().length > 0;
